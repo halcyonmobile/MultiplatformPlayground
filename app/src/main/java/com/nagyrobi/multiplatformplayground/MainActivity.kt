cd34de5de0896934d4ba1f3a198bb18603349960
Nagy Robi <nagyrobi144@gmail.com>
2019-11-01 09:38:04 +0200

    Initialize project

---
+++ app/src/main/java/com/nagyrobi/multiplatformplayground/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nagyrobi.multiplatformplayground
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
