e20d3fa49bf1d23be03a05fbcf5706bc6ff4d93a
Nagy Robi <nagyrobi144@gmail.com>
2019-11-11 11:12:46 +0200

    Add basic api implementations

--- backend/src/com.halcyonmobile.multiplatformplayground/backend/Api.kt
+++ backend/src/com.halcyonmobile.multiplatformplayground/backend/Api.kt
@@ -1,5 +1,6 @@
 package com.halcyonmobile.multiplatformplayground.backend
 
+import com.halcyonmobile.multiplatformplayground.model.Application
 import com.halcyonmobile.multiplatformplayground.storage.Database
 import io.ktor.application.call
 import io.ktor.http.HttpStatusCode
@@ -9,32 +10,42 @@ import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.put
+import java.lang.Exception
 
+// todo general error handling
 internal fun Routing.api(database: Database) {
-
+    apiApplications(database)
+    apiCreateApplication(database)
+    apiUpdateApplication(database)
+    apiGetApplication(database)
+    apiFilterApplications(database)
+    apiGetCategories(database)
+    apiPostScreenshot(database)
 }
 
 /**
  * GET /api/v1/applications
  */
-private fun Routing.apiApplications() {
+private fun Routing.apiApplications(database: Database) {
     get("/applications") {
-        call.respond(HttpStatusCode.OK)
+        database.getApplications().let {
+            call.respond(it)
+        }
     }
 }
 
 /**
  *  POST /api/v1/applications
  */
-private fun Routing.apiCreateApplication() {
+private fun Routing.apiCreateApplication(database: Database) {
     post("/applications") {
-        //       call.receive(Application)
-//        todo save application
-        val created = true
-        if (created) {
-            call.respond(HttpStatusCode.Created)
-        } else {
-            call.respond(HttpStatusCode.Conflict)
+        call.receive<Application>().let {
+            try {
+                database.createApplication(it)
+                call.respond(HttpStatusCode.Created)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.Conflict)
+            }
         }
     }
 }
@@ -42,15 +53,15 @@ private fun Routing.apiCreateApplication() {
 /**
  * PUT /api/v1/applications
  */
-private fun Routing.apiUpdateApplication() {
+private fun Routing.apiUpdateApplication(database: Database) {
     put("/applications") {
-        // call.receive(Application)
-        // update
-        val updated = true
-        if (updated) {
-            call.respond(HttpStatusCode.OK)
-        } else {
-            call.respond(HttpStatusCode.Conflict)
+        call.receive<Application>().let {
+            try {
+                database.updateApplication(it)
+                call.respond(HttpStatusCode.OK)
+            } catch (e: Exception) {
+                call.respond(HttpStatusCode.Conflict)
+            }
         }
     }
 }
@@ -58,9 +69,12 @@ private fun Routing.apiUpdateApplication() {
 /**
  * GET /api/v1/applications/:id
  */
-private fun Routing.apiGetApplication() {
+private fun Routing.apiGetApplication(database: Database) {
     get("/applications/{id}") {
-        val id = call.parameters["id"]
+        val id = call.parameters["id"].toString().toLong()
+        database.getApplication(id).let {
+            call.respond(it)
+        }
         // return application
     }
 }
@@ -68,22 +82,36 @@ private fun Routing.apiGetApplication() {
 /**
  * GET /api/v1/applications/filter
  */
-// TODO add this
+private fun Routing.apiFilterApplications(database: Database) {
+    get("/applications/filter") {
+        val name = call.request.queryParameters[NAME_QUERY_KEY].toString()
+        val categoryId = call.request.queryParameters[CATEGORY_QUERY_KEY]!!.toLong()
+
+        database.getApplications(name, categoryId).let {
+            call.respond(it)
+        }
+    }
+}
 
 /**
  * GET /api/v1/categories
  */
-private fun Routing.apiGetCategories() {
+private fun Routing.apiGetCategories(database: Database) {
     get("/categories") {
-        // todo return categories
+        database.getCategories().let {
+            call.respond(it)
+        }
     }
 }
 
 /**
  * POST /api/v1/screenshots
  */
-private fun Routing.apiPostScreenshot() {
+private fun Routing.apiPostScreenshot(database: Database) {
     post("/screenshots") {
-        // todo handle screenshot
+        // todo handle file
     }
 }
+
+const val NAME_QUERY_KEY = "name"
+const val CATEGORY_QUERY_KEY = "category"
