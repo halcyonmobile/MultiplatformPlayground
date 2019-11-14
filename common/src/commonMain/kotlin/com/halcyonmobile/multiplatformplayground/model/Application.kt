8b00c415fb0ddd6bb4f7c926acc140c25c716e60
Nagy Robi <nagyrobi144@gmail.com>
2019-11-14 14:33:11 +0200

    Remove deprecated optional values

--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/model/Application.kt
+++ common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/model/Application.kt
@@ -1,6 +1,5 @@
 package com.halcyonmobile.multiplatformplayground.model
 
-import kotlinx.serialization.Optional
 import kotlinx.serialization.Serializable
 
 @Serializable
@@ -8,27 +7,16 @@ data class Application(
     val id: Long,
     val name: String,
     val developer: String,
-    @Optional
     val icon: String? = null,
-    @Optional
     val rating: Float? = null,
-    @Optional
     val ratingCount: Int? = null,
-    @Optional
     val storeUrl: String? = null,
-    @Optional
     val description: String? = null,
-    @Optional
     val downloads: String? = null,
-    @Optional
     val version: String? = null,
-    @Optional
     val size: String? = null,
-    @Optional
     val favourite: Boolean = false,
-    @Optional
     var category: Category? = null,
-    @Optional
     var screenshots: List<Screenshot>? = null
 
 )
