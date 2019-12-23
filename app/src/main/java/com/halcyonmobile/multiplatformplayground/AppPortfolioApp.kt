6bc5b19f352d9c4e3845f7ac6625d765aaa5c852
Nagy Robi <nagyrobi144@gmail.com>
2019-12-23 17:47:48 +0200

    Add kodein

--- app/src/main/java/com/halcyonmobile/multiplatformplayground/AppPortfolioApp.kt
+++ app/src/main/java/com/halcyonmobile/multiplatformplayground/AppPortfolioApp.kt
@@ -1,12 +1,18 @@
 package com.halcyonmobile.multiplatformplayground
 
 import android.app.Application
+import org.kodein.di.Kodein
+import org.kodein.di.KodeinAware
 
-class AppPortfolioApp: Application(){
+class AppPortfolioApp : Application(), KodeinAware {
 
+    override val kodein = Kodein.lazy {
+        // todo bindings
+    }
 
     override fun onCreate() {
         super.onCreate()
         // todo setup Kodein
     }
+
 }
