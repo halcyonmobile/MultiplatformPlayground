ea2f47f7dda39332e81aaaf9a923e33a0e691190
Nagy Robi <nagyrobi144@gmail.com>
2020-01-29 09:19:16 +0200

    Implement actual observe methods with lambdas

--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/observer/Observable.kt
+++ common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/observer/Observable.kt
@@ -4,17 +4,11 @@ expect class Observable<T : Any>() {
     var value: T?
 
     fun observe(observer: Observer<T>)
+    fun observe(doOnChange: (T?) -> Unit)
     fun removeObserver(observer: Observer<T>)
 }
 
 fun <T : Any> observableOf(value: T) = Observable<T>().also { it.value = value }
-
-fun <T : Any> Observable<T>.observe(doOnChange: (T?) -> Unit) =
-    observe(object : Observer<T>() {
-        override fun onChanged(value: T?) {
-            doOnChange(value)
-        }
-    })
 
 fun <T : Any, R : Any> Observable<T>.map(mapper: (T) -> (R)) =
     Observable<R>().also { newObservable ->
--- common/src/iosMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/observer/Observable.kt
+++ common/src/iosMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/observer/Observable.kt
@@ -17,4 +17,11 @@ actual class Observable<T : Any> {
     actual fun removeObserver(observer: Observer<T>) {
         observers.remove(observer)
     }
+
+    actual fun observe(doOnChange: (T?) -> Unit) =
+        observe(object : Observer<T>() {
+            override fun onChanged(value: T?) {
+                doOnChange(value)
+            }
+        })
 }
