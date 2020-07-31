4a79ba7a51ce03d74e6d32546841a6339ba08400
Péter Pandula <peter@halcyonmobile.com>
2020-07-31 14:37:28 +0300

    Use the Ktor logger from Beagle.

--- common/src/androidMain/kotlin/com/halcyonmobile/multiplatformplayground/api/KtorEngine.kt
+++ common/src/androidMain/kotlin/com/halcyonmobile/multiplatformplayground/api/KtorEngine.kt
@@ -2,12 +2,16 @@ package com.halcyonmobile.multiplatformplayground.api
 
 import android.util.Log
 import com.pandulapeter.beagle.log.BeagleLogger
+import com.pandulapeter.beagle.logKtor.BeagleKtorLogger
 import io.ktor.client.HttpClientConfig
 import io.ktor.client.engine.android.Android
+import io.ktor.client.features.HttpClientFeature
 
 internal actual val engine by lazy { Android.create() }
 
-internal actual val installNetworkLogger: HttpClientConfig<*>.() -> Unit = { install(BetterBeagleLogger) }
+internal actual val installNetworkLogger: HttpClientConfig<*>.() -> Unit = {
+    (BeagleKtorLogger.logger as? HttpClientFeature<*, *>?)?.let { install(it) }
+}
 
 actual val log: (String) -> Unit = {
     BeagleLogger.log(it)
