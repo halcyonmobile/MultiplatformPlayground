da055668666a609a0b501d9151bd530768a20396
Zsolt Boldizsár <zsolt@halcyonmobile.com>
2020-08-04 15:45:12 +0300

    Add example usage of logging from shared module
    #16

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -1,4 +1,5 @@
 import UIKit
+import common
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -6,6 +7,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        LoggingKt.log("Example usage of logging from the common module.")
         return true
     }
 
