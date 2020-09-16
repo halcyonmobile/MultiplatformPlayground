18cd7d8fed6e0e2cba9412ed3147e7ffd911fa88
Nagy Robi <nagyrobi144@gmail.com>
2020-09-16 09:26:40 +0300

    Replace the Observable abstraction with StateFlow

--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/viewmodel/HomeViewModel.kt
+++ common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/viewmodel/HomeViewModel.kt
@@ -5,38 +5,44 @@ import com.halcyonmobile.multiplatformplayground.model.Category
 import com.halcyonmobile.multiplatformplayground.repository.category.CategoryRepository
 import com.halcyonmobile.multiplatformplayground.shared.CoroutineViewModel
 import com.halcyonmobile.multiplatformplayground.shared.Result
-import com.halcyonmobile.multiplatformplayground.shared.observer.Observable
 import com.halcyonmobile.multiplatformplayground.usecase.FetchCategoriesUseCase
 import com.halcyonmobile.multiplatformplayground.usecase.GetCategoriesUseCase
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalCoroutinesApi::class)
 class HomeViewModel internal constructor(
     private val getCategories: GetCategoriesUseCase,
     private val fetchCategories: FetchCategoriesUseCase,
     private val categoryRepository: CategoryRepository
 ) : CoroutineViewModel() {
 
-    val categories = Observable<List<Category>>()
-    val error = Observable<String>()
+    private val _categories = MutableStateFlow(emptyList<Category>())
+    val categories: StateFlow<List<Category>> = _categories
+
+    private val _error = MutableStateFlow<String?>(null)
+    val error: StateFlow<String?> = _error
 
     init {
         coroutineScope.launch {
             categoryRepository.categories.collect {
                 log("Categories stream: $it")
-                categories.value = it
+                _categories.value = it
             }
         }
         coroutineScope.launch {
             when (val result = getCategories()) {
-                is Result.Error -> error.value = result.exception.message
+                is Result.Error -> _error.value = result.exception.message
             }
         }
     }
 
     fun fetch() = coroutineScope.launch {
         when (val result = fetchCategories()) {
-            is Result.Error -> error.value = result.exception.message
+            is Result.Error -> _error.value = result.exception.message
         }
     }
 
