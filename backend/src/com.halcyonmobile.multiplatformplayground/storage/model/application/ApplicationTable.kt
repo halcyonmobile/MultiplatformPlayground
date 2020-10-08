dbb1462ba83a5f67732459f164d9feddfdf7e767
Nagy Robi <nagyrobi144@gmail.com>
2020-10-08 14:51:45 +0300

    Make sure the new exposed DAO functionality is used on backend

--- backend/src/com.halcyonmobile.multiplatformplayground/storage/model/application/ApplicationTable.kt
+++ backend/src/com.halcyonmobile.multiplatformplayground/storage/model/application/ApplicationTable.kt
@@ -8,14 +8,14 @@ import org.jetbrains.exposed.sql.ReferenceOption
 internal object ApplicationTable : IntIdTable("applications") {
     val name = varchar(APP_NAME, 50)
     val developer = varchar(APP_DEVELOPER, 50)
-    val icon = varchar(APP_ICON, 255).nullable()
-    val rating = decimal(APP_RATING, 3, 1).nullable()
-    val ratingCount = integer(APP_RATING_COUNT).nullable()
-    val storeUrl = varchar(APP_STORE_URL, 255).nullable()
-    val description = varchar(APP_DESCRIPTION, 255).nullable()
-    val downloads = varchar(APP_DOWNLOADS, 50).nullable()
-    val version = varchar(APP_VERSION, 50).nullable()
-    val size = varchar(APP_SIZE, 50).nullable()
+    val icon = varchar(APP_ICON, 255)
+    val rating = decimal(APP_RATING, 3, 1)
+    val ratingCount = integer(APP_RATING_COUNT)
+    val storeUrl = varchar(APP_STORE_URL, 255)
+    val description = varchar(APP_DESCRIPTION, 255)
+    val downloads = varchar(APP_DOWNLOADS, 50)
+    val version = varchar(APP_VERSION, 50)
+    val size = varchar(APP_SIZE, 50)
     val favourite = bool(APP_FAVOURITE).default(false)
     val category = reference(
         APP_CATEGORY_ID,
--- backend/src/com.halcyonmobile.multiplatformplayground/storage/model/screenshot/ScreenshotEntity.kt
+++ backend/src/com.halcyonmobile.multiplatformplayground/storage/model/screenshot/ScreenshotEntity.kt
@@ -1,5 +1,6 @@
 package com.halcyonmobile.multiplatformplayground.storage.model.screenshot
 
+import com.halcyonmobile.multiplatformplayground.model.Screenshot
 import com.halcyonmobile.multiplatformplayground.storage.model.application.ApplicationEntity
 import org.jetbrains.exposed.dao.IntEntity
 import org.jetbrains.exposed.dao.IntEntityClass
@@ -8,7 +9,9 @@ import org.jetbrains.exposed.dao.id.EntityID
 class ScreenshotEntity(id: EntityID<Int>) : IntEntity(id) {
     companion object : IntEntityClass<ScreenshotEntity>(ScreenshotTable)
 
-    val name by ScreenshotTable.name
-    val image by ScreenshotTable.image
+    var name by ScreenshotTable.name
+    var image by ScreenshotTable.image
     var application by ApplicationEntity referencedOn ScreenshotTable.application
 }
+
+fun ScreenshotEntity.toScreenshot() = Screenshot(id.value.toLong(), name, image)
