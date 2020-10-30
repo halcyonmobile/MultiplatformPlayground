e0aad514b60cc0c7290a531951921dce9bb9e454
Nagy Robi <nagyrobi144@gmail.com>
2020-10-30 12:05:24 +0200

    Show categories on iOS

--- iosApp/iosApp/AppDelegate.swift
+++ iosApp/iosApp/AppDelegate.swift
@@ -6,7 +6,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        LoggingKt.log("Example usage of logging from the common module.")
         return true
     }
 
