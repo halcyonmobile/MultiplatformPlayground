50b69932d0c8570f0d5f6b5dffd06b4a6ef32c7c
Nagy Robi <nagyrobi144@gmail.com>
2020-11-02 15:41:23 +0200

    Implement UploadApplicationState

--- common/src/iosMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/util/ServiceLocator.kt
+++ common/src/iosMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/util/ServiceLocator.kt
@@ -7,19 +7,33 @@ import com.halcyonmobile.multiplatformplayground.repository.category.CategoryRem
 import com.halcyonmobile.multiplatformplayground.repository.category.CategoryRepository
 import com.halcyonmobile.multiplatformplayground.usecase.FetchCategoriesUseCase
 import com.halcyonmobile.multiplatformplayground.usecase.GetCategoriesUseCase
+import com.halcyonmobile.multiplatformplayground.usecase.GetCategoryUseCase
+import com.halcyonmobile.multiplatformplayground.usecase.CreateApplicationUseCase
 import com.halcyonmobile.multiplatformplayground.viewmodel.HomeViewModel
-
+import com.halcyonmobile.multiplatformplayground.viewmodel.UploadApplicationViewModel
+import com.halcyonmobile.multiplatformplayground.repository.application.ApplicationRepository
+import com.halcyonmobile.multiplatformplayground.repository.application.ApplicationRemoteSource
+import com.halcyonmobile.multiplatformplayground.api.ApplicationApi
 // TODO create a proper solution
 class ServiceLocator {
 
-    fun getDatabase() = DatabaseFactory.getInstance().create()
+    private val database = DatabaseFactory.getInstance().create()
+    private val categoryRepository = CategoryRepository(
+        CategoryRemoteSource(CategoryApi()),
+        CategoryLocalSource(database)
+    )
+    private val applicationRepository = ApplicationRepository(ApplicationRemoteSource(ApplicationApi()))
 
     // 🤢🤮
     fun getHomeViewModel(): HomeViewModel =
-        CategoryRepository(
-            CategoryRemoteSource(CategoryApi()),
-            CategoryLocalSource(getDatabase())
-        ).let {
-            HomeViewModel(GetCategoriesUseCase(it), FetchCategoriesUseCase(it))
-        }
+        HomeViewModel(
+            GetCategoriesUseCase(categoryRepository),
+            FetchCategoriesUseCase(categoryRepository)
+        )
+
+    fun getUploadApplicationViewModel(categoryId: Long): UploadApplicationViewModel = UploadApplicationViewModel(
+        categoryId,
+        GetCategoryUseCase(categoryRepository),
+        CreateApplicationUseCase(applicationRepository)
+    )
 }
