79136cdcce50eb20ba4e9af0c590787e9454849a
Nagy Robi <nagyrobi144@gmail.com>
2020-11-03 09:00:11 +0200

    Create Localizations.kt to make common localizations accessible for iOS

---
+++ common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/Localizations.kt
@@ -0,0 +1,25 @@
+package com.halcyonmobile.multiplatformplayground.shared
+
+import dev.icerock.moko.resources.desc.Resource
+import dev.icerock.moko.resources.desc.StringDesc
+import com.halcyonmobile.multiplatformplayground.MR
+
+val appName = StringDesc.Resource(MR.strings.app_name)
+val retry = StringDesc.Resource(MR.strings.retry)
+
+//    Bottom Navigation
+val home = StringDesc.Resource(MR.strings.home)
+val favourites = StringDesc.Resource(MR.strings.favourites)
+val settings = StringDesc.Resource(MR.strings.settings)
+
+//    Application
+val description = StringDesc.Resource(MR.strings.description)
+val showMore = StringDesc.Resource(MR.strings.show_more)
+val showLess = StringDesc.Resource(MR.strings.show_less)
+val rating = StringDesc.Resource(MR.strings.rating)
+val downloads = StringDesc.Resource(MR.strings.downloads)
+val screenshots = StringDesc.Resource(MR.strings.screenshots)
+val applicationDetails = StringDesc.Resource(MR.strings.application_details)
+val category = StringDesc.Resource(MR.strings.category)
+val developer = StringDesc.Resource(MR.strings.developer)
+
