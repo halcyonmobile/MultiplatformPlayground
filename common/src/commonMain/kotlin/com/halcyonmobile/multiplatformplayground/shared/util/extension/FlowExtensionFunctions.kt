af94f4d1d4f8c600acfbf33f2fcf52a5ea39d10d
Nagy Robi <nagyrobi144@gmail.com>
2020-11-24 13:12:09 +0200

    Remove unused classes

--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/util/extension/FlowExtensionFunctions.kt
+++
@@ -1,8 +0,0 @@
-package com.halcyonmobile.multiplatformplayground.shared.util.extension
-
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.flattenMerge
-import kotlinx.coroutines.flow.flowOf
-
-fun <T> Flow<T>.merge(vararg other: Flow<T>): Flow<T> = flowOf(this, *other).flattenMerge()
