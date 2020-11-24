24ff89505deebb9a96bc0751bd0b6a33865659ca
Nagy Robi <nagyrobi144@gmail.com>
2020-11-24 13:12:09 +0200

    Remove unused classes

--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/shared/util/extension/CollectionExtensionFunctions.kt
+++
@@ -1,4 +0,0 @@
-package com.halcyonmobile.multiplatformplayground.shared.util.extension
-
-fun <T> List<T>.safeSubList(fromIndex: Int, toIndex: Int) =
-    drop(fromIndex).take(toIndex - fromIndex)
--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/usecase/CreateApplicationUseCase.kt
+++ common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/usecase/CreateApplicationUseCase.kt
@@ -4,11 +4,9 @@ import com.halcyonmobile.multiplatformplayground.model.UploadApplicationModel
 import com.halcyonmobile.multiplatformplayground.repository.application.ApplicationRepository
 import com.halcyonmobile.multiplatformplayground.shared.Result
 
-internal class CreateApplicationUseCase (private val applicationRepository: ApplicationRepository) {
+internal class CreateApplicationUseCase(private val applicationRepository: ApplicationRepository) {
 
-    suspend operator fun invoke(
-        uploadApplicationModel: UploadApplicationModel
-    ) = Result {
+    suspend operator fun invoke(uploadApplicationModel: UploadApplicationModel) = Result {
         applicationRepository.create(uploadApplicationModel)
     }
 }
