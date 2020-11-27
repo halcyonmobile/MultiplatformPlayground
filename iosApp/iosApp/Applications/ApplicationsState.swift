807d36fd6ac1efd0e7b72fedd2dab769e229dde1
Nagy Robi <nagyrobi144@gmail.com>
2020-11-27 12:16:22 +0200

    Update state and event handling for applications

--- iosApp/iosApp/Applications/ApplicationsState.swift
+++ iosApp/iosApp/Applications/ApplicationsState.swift
@@ -16,15 +16,28 @@ class ApplicationsState: ObservableObject{
     
     @Published private(set) var applications = [ApplicationUiModel.App]()
     @Published private(set) var isLoading = true
+    @Published private(set) var state: ApplicationsViewModel.State = ApplicationsViewModel.State.normal
     
     init(categoryId: Int64) {
         self.categoryId = categoryId
         viewModel = ServiceLocator().getApplicationsViewModel(categoryId: categoryId)
         
         // TODO handle other types like loading
-        viewModel.observeApplications { items in
+        viewModel.observeItems { items in
             self.applications = items.filter { $0 is ApplicationUiModel.App}.map { $0 as! ApplicationUiModel.App}
             self.isLoading = items.contains{ $0 is ApplicationUiModel.Loading}
+        }
+        viewModel.observeState{ state in
+            self.state = state
+        }
+        viewModel.observeEvent{ event in
+            switch event {
+            case ApplicationsViewModel.Event.error:
+                // TODO send out error
+                break
+            default:
+                break
+            }
         }
     }
     
