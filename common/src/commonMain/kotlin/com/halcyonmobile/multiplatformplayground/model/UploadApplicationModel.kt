c8ef044d8330c9acb95faebffbee9ef3529185ea
nagyrobi144 <nagyrobi144@gmail.com>
2020-12-10 12:53:39 +0200

    Implement UploadApplication on iOS

--- common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/model/UploadApplicationModel.kt
+++ common/src/commonMain/kotlin/com/halcyonmobile/multiplatformplayground/model/UploadApplicationModel.kt
@@ -1,14 +1,14 @@
 package com.halcyonmobile.multiplatformplayground.model
 
-import com.halcyonmobile.multiplatformplayground.shared.util.File
+import com.halcyonmobile.multiplatformplayground.shared.util.ImageFile
 
 data class UploadApplicationModel(
     val name: String,
     val developer: String,
-    val icon: File,
+    val icon: ImageFile,
     val rating: Float,
     val description: String,
     val downloads: String,
     val categoryId: Long,
-    val screenshots: List<File>
+    val screenshots: List<ImageFile>
 )
