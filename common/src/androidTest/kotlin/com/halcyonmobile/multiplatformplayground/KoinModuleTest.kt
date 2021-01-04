bbc73bd311d9b536a702993c68c2be1ef26be3cf
nagyrobi144 <nagyrobi144@gmail.com>
2021-01-04 12:46:36 +0200

    Implement KoinBaseTest.kt

--- common/src/androidTest/kotlin/com/halcyonmobile/multiplatformplayground/KoinModuleTest.kt
+++ common/src/androidTest/kotlin/com/halcyonmobile/multiplatformplayground/KoinModuleTest.kt
@@ -1,34 +1,14 @@
 package com.halcyonmobile.multiplatformplayground
 
-import android.content.Context
-import com.halcyonmobile.multiplatformplayground.di.initKoin
-import com.halcyonmobile.multiplatformplayground.util.BaseTest
-import io.mockk.MockKAnnotations
-import io.mockk.impl.annotations.RelaxedMockK
-import org.junit.Before
+import com.halcyonmobile.multiplatformplayground.util.KoinBaseTest
 import org.junit.experimental.categories.Category
-import org.koin.core.context.loadKoinModules
-import org.koin.dsl.module
-import org.koin.test.KoinTest
 import org.koin.test.category.CheckModuleTest
 import org.koin.test.check.checkModules
 import kotlin.test.Test
 
 @Category(CheckModuleTest::class)
-class KoinModuleTest : BaseTest(), KoinTest {
-
-    @RelaxedMockK
-    private lateinit var context: Context
-
-    @Before
-    fun setup() {
-        MockKAnnotations.init(this)
-    }
+class KoinModuleTest : KoinBaseTest() {
 
     @Test
-    fun checkCommonModules() = initKoin {
-        loadKoinModules(
-            module { factory { context } }
-        )
-    }.checkModules()
+    fun checkCommonModules() = koinTestRule.koin.checkModules()
 }
