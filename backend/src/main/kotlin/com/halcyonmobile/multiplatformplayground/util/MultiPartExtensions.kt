94a558e91da13cfb99d27ffd16537ad77bbf0e13
nagyrobi144 <nagyrobi144@gmail.com>
2021-01-11 12:56:37 +0200

    Remove hardcoded .png extension

--- backend/src/main/kotlin/com/halcyonmobile/multiplatformplayground/util/MultiPartExtensions.kt
+++ backend/src/main/kotlin/com/halcyonmobile/multiplatformplayground/util/MultiPartExtensions.kt
@@ -4,7 +4,7 @@ import io.ktor.http.content.*
 import java.io.File
 import java.time.LocalDateTime
 
-fun PartData.FileItem.toFile() = File("${originalFileName!!}.png").also { file ->
+fun PartData.FileItem.toFile() = File(originalFileName!!).also { file ->
     streamProvider().use { input ->
         file.outputStream().buffered().use { output ->
             input.copyTo(output)
